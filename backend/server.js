d86136faf69b18ed6d9845258fa64708b13c6f19
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-14 19:27:35 +0530

    add mongodb string and port into .env fiile and create .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res)=>{
-    res.send("server running")
+    res.send("API running")
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
-    console.log(`server running ${PORT}`)
+    console.log(`server running on port ${PORT}`)
 });
