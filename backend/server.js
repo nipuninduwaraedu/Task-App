36e20eac66d048afd270d800b56e50bf44ea3549
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-14 19:20:24 +0530

    create server.js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from  "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send("server running")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server running ${PORT}`)
+});
