c09bd000363f5b4e64128fe18f4021502af108c5
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-15 21:59:01 +0530

    create in controller.js  createtask

---
+++ backend/controllers/taskControllers.js
@@ -0,0 +1,16 @@
+import "../models/task.js";
+
+const createTask = async (req, res) => {
+  try {
+    const { titel } = req.body;
+
+    const task = await task.create({
+      titel,
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
